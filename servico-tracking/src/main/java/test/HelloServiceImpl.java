5476dca7efb18d193f894bca9001c8aa7ed07630
Leandro Aparecido <leandro.aparecido@groupon.com.br>
2012-05-14 15:20:36 -0300

    updates to make it deploy

--- servico-tracking/src/main/java/test/HelloServiceImpl.java
+++ servico-tracking/src/main/java/test/HelloServiceImpl.java
@@ -1,12 +1,17 @@
 package test;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Schedule;
 import javax.ejb.Stateless;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
+import org.springframework.data.repository.core.support.RepositoryFactorySupport;
 
 import test.repository.TestRepository;
 
@@ -18,15 +23,17 @@ public class HelloServiceImpl implements HelloService {
 	@Inject
 	Event<Integer> updateEvent;
 
-	@Inject
+	@PersistenceContext
+	EntityManager entityManager;
+
 	TestRepository repository;
 
-/*	@PostConstruct
+	@PostConstruct
 	public void init() {
 		RepositoryFactorySupport support = new JpaRepositoryFactory(entityManager);
-		repository = support.getRepository(VoucherRepository.class);
+		repository = support.getRepository(TestRepository.class);
 	}
-*/
+
 	public String sayHello(String to) {
 		logger.debug("Saying hello to [{}]!", to);
 		repository.save(new Person(to));
